188eb77ef69c217b1be56f73351b566de6a7b73b
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-02-15 20:12:26 +0900

    init

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+require './togetter2text'
+
+module Togetter2Text
+  class Application < Sinatra::Application
+    register Sinatra::Reloader
+    enable :inline_templates
+    include ERB::Util
+
+    get '/' do
+      erb :index
+    end
+    post '/extract' do
+      #url = "http://togetter.com/li/782106"
+      url = h(params['url'])
+      tg2t = Togetter2Text::Extractor.new
+      erb tg2t.get_texts(url).join("<br>")
+    end
+  end # class
+end # module
+
+__END__
+@@layout
+<h1>Togetter2Text</h1>
+<hr>
+<%= yield %>
+</hr>
+
+
+@@index
+<form action="extract" method="post">
+<input type="text" name="url" size=80>
+<input type="submit">
+</form>
+
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './app'
+
+run Togetter2Text::Application
